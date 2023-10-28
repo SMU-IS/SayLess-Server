14389cded40c0d6afa5200409cf674eaff153d83
Derrick-lkh <derrick872k@gmail.com>
2023-10-29 00:41:39 +0800

    manual signup - add name, pic

--- src/middlewares/firebaseauth.middleware.js
+++ src/middlewares/firebaseauth.middleware.js
@@ -1,5 +1,8 @@
 const { fbauth } = require("../config/firebaseConfig");
 
+const imageURL =
+  "https://i.kym-cdn.com/entries/icons/original/000/036/007/underthewatercover.jpg";
+
 async function firebaseValidate(req, res, next) {
   if (!req.body.accessTokenId) {
     return res.status(403).send("TOKEN NOT FOUND");
@@ -8,7 +11,11 @@ async function firebaseValidate(req, res, next) {
   await fbauth
     .verifyIdToken(token)
     .then(async (decodedToken) => {
-      const { email, name, picture, user_id } = decodedToken;
+      let { email, name, picture, user_id } = decodedToken;
+      if (!name) {
+        name = email.split("@")[0];
+        picture = imageURL;
+      }
       req.fbuser = {
         userId: user_id,
         email: email,
