595ee697824885edf11aef31968f4a3f94293e68
Derrick-lkh <derrick872k@gmail.com>
2023-10-29 22:40:18 +0800

    notification - chat

--- src/routes/message.route.js
+++ src/routes/message.route.js
@@ -6,7 +6,7 @@ const axios = require("axios");
 const ObjectId = require("mongodb").ObjectId;
 const { Server } = require("socket.io");
 const auth = require("../middlewares/io_auth.middleware");
-const { Chat } = require("../models");
+const { Chat, Chatroom } = require("../models");
 
 const io = new Server(server, {
   cors: {
@@ -62,13 +62,89 @@ io.use(auth).on("connection", async (socket) => {
         })
           .populate("sender")
           .then((updatedChat) => {
-            console.log(updatedChat);
             io.to(chatroom).emit("messageData", updatedChat);
           });
+        let filter = {
+          _id: new ObjectId(chatroom),
+        };
+        Chatroom.aggregate([
+          {
+            $match: filter, // Apply your initial match criteria
+          },
+          {
+            $lookup: {
+              from: "listings", // Replace 'listings' with the actual name of the referenced collection
+              localField: "listing",
+              foreignField: "_id",
+              as: "listing",
+            },
+          },
+          {
+            $lookup: {
+              from: "users", // Replace 'users' with the actual name of the referenced collection
+              localField: "participants",
+              foreignField: "_id",
+              as: "participants",
+            },
+          },
+          {
+            $project: {
+              _id: 0, // Exclude the _id field
+              id: "$_id", // Rename _id to id for the chatroom document
+              createdOn: 1, // Include createdOn field as is
+              // Other fields
+              debug: "$participants",
+              participants: {
+                $filter: {
+                  input: "$participants",
+                  as: "participant",
+                  cond: {
+                    $ne: ["$$participant._id", new ObjectId(socket.user._id)],
+                  },
+                },
+              },
+
+              listing: 1, // Include the populated listing field as is
+            },
+          },
+          // Continue with the rest of your aggregation stages
+        ]).then((result) => {
+          let notifyReceiver = result[0].participants[0]._id.toString(); // User on the receiveing end
+          io.to(notifyReceiver).emit("notiMessage", {
+            type: "message",
+            message: `you have received a new message from ${socket.user.name}`,
+          });
+        });
       });
     } catch (err) {
       socket.emit("error", "Message failed to send");
     }
+  });
+
+  socket.on("chatNotification", () => {
+    // Emitter: notiMessage
+    // {
+    //   type: "message"
+    // }
+    let userDetails = socket.user._id.toString();
+    socket.join(userDetails);
+    // io.to(userDetails).emit("notification", {
+    //   type: "message",
+    //   message: "you have a new message",
+    // });
+  });
+
+  socket.on("expiryNotification", () => {
+    // Emitter: notiExpiry
+    // {
+    //   type: "expiry"
+    // }
+    let userDetails = socket.user._id.toString();
+    socket.join(userDetails);
+    // io.to(userDetails).emit("notification", {
+    //   type: "message",
+    //   message: "you have a new message",
+    // });
   });
 });
 
