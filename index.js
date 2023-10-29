1933011e405df8e77cc16030613daf171dbf595d
Derrick-lkh <derrick872k@gmail.com>
2023-10-30 02:07:26 +0800

    test pm2

--- index.js
+++ index.js
@@ -24,47 +24,47 @@ mongoose.connect(process.env.MONGO_DB_URL).then(() => {
   });
 });
 
-const exitHandler = () => {
-  if (server) {
-    server.close(() => {
-      console.log("Server closed");
-      if (socketServer) {
-        socketServer.close(() => {
-          console.log("Socket server closed");
-          process.exit(1); // Exit with a success code
-        });
-      } else {
-        process.exit(1); // No socket server to close, just exit with a success code
-      }
-    });
-  } else {
-    if (socketServer) {
-      socketServer.close(() => {
-        console.log("Socket server closed");
-        process.exit(1); // Exit with a success code
-      });
-    } else {
-      process.exit(1); // No server or socket server to close, just exit with a success code
-    }
-  }
-};
+// const exitHandler = () => {
+//   if (server) {
+//     server.close(() => {
+//       console.log("Server closed");
+//       if (socketServer) {
+//         socketServer.close(() => {
+//           console.log("Socket server closed");
+//           process.exit(1); // Exit with a success code
+//         });
+//       } else {
+//         process.exit(1); // No socket server to close, just exit with a success code
+//       }
+//     });
+//   } else {
+//     if (socketServer) {
+//       socketServer.close(() => {
+//         console.log("Socket server closed");
+//         process.exit(1); // Exit with a success code
+//       });
+//     } else {
+//       process.exit(1); // No server or socket server to close, just exit with a success code
+//     }
+//   }
+// };
 
-const unexpectedErrorHandler = (error) => {
-  console.error(error);
-  exitHandler();
-};
+// const unexpectedErrorHandler = (error) => {
+//   console.error(error);
+//   exitHandler();
+// };
 
-process.on("uncaughtException", unexpectedErrorHandler);
-process.on("unhandledRejection", unexpectedErrorHandler);
+// process.on("uncaughtException", unexpectedErrorHandler);
+// process.on("unhandledRejection", unexpectedErrorHandler);
 
-process.on("SIGTERM", () => {
-  console.log("SIGTERM received");
-  process.exit(0);
-  // exitHandler();
-});
+// process.on("SIGTERM", () => {
+//   console.log("SIGTERM received");
+//   process.exit(0);
+//   // exitHandler();
+// });
 
-process.on("SIGINT", () => {
-  console.log("Ctrl+C (SIGINT) received");
-  process.exit(0);
-  // exitHandler();
-});
+// process.on("SIGINT", () => {
+//   console.log("Ctrl+C (SIGINT) received");
+//   process.exit(0);
+//   // exitHandler();
+// });
